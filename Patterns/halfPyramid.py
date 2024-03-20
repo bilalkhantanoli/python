3dcea36a3123455030415c4e2952c3127c106a93
Bilal Aziz <numl-s22-12915@numls.edu.pk>
2024-03-20 21:47:50 +0500

    Pattern of Half Pyramid by Number

---
+++ Patterns/halfPyramid.py
@@ -0,0 +1,17 @@
+def halfPyramid(n):
+    for i in range(1,n+1):
+        for j in range(1,i+1):
+            print(j,end='')
+        print()
+
+halfPyramid(5)
+
+
+#output
+'''
+1
+12
+123
+1234
+12345
+'''
